935c22a168559ceffa618e3dbd4625a76cbd4ca5
jacquelineoura <jj00ura@gmail.com>
2020-07-13 22:40:53 -0300

    eliminacion de codigo comentado
    *eliminacion de codigo comentado
    * y algunos hardcodeos

--- Programa.cpp
+++ Programa.cpp
@@ -1,6 +1,6 @@
 #include "Programa.h"
 
-
+const string MSJ_ERROR_VISTAS = "No se puede mostrar vistas porque no se encontro el archivo anteriormente" ;
 
 Programa::Programa(bool esta_vistas) {
     this -> esta_vistas = esta_vistas ;
@@ -14,7 +14,7 @@ int Programa:: obtener_opcion() {
     return opcion ;
 }
 
-void Programa:: mostrar_menu() {   //aca pegue el menu que habias hecho Alex
+void Programa:: mostrar_menu() {
         cout << endl << endl << endl ;
         cout << "\t***************  MENU **************"<< endl << endl ;
         cout << "\tDecida que operacion quiere realizar: " << endl << endl ;
@@ -29,39 +29,39 @@ void Programa:: elegir_opcion(){
     cout << MSJ_OPCION ;
     cin >> opcion_menu ;
     cout << endl ;
-    opcion = atoi ( opcion_menu.c_str() ) ;    //falta validar opcion_menu  - FALTA
+    opcion = atoi ( opcion_menu.c_str() ) ;
 }
 
 
-void Programa::abrir_menu_interno(Lista<Peliculas*> &lista_no_vistas , Lista<Peliculas*> &lista_vistas ){
+void Programa:: abrir_menu_interno(Lista<Peliculas*> &lista_no_vistas , Lista<Peliculas*> &lista_vistas ){
 
     switch ( opcion ){
             case PELIS_VISTAS: {
-                if ( esta_vistas == true ){
-                    mostrar_lista_pelicula(lista_vistas) ;
-                }else cout << endl << "No se puede mostrar vistas porque no se encontro el archivo anteriormente" << endl ;
-            }
-            break ;
+                                if ( esta_vistas == true ){
+                                    mostrar_lista_pelicula(lista_vistas) ;
+                                }else cout << endl << MSJ_ERROR_VISTAS << endl ;
+                                }
+                                break ;
 
             case PELIS_NO_VISTAS: mostrar_lista_pelicula(lista_no_vistas) ;
-                break ;
+                                  break ;
 
             case PELIS_RECOMENDADAS:{
-                Lista < Dato > lista_recomendadas ;
+                                    Lista < Dato > lista_recomendadas ;
 
-                if (esta_vistas)
+                                    if (esta_vistas)
 
-                    llenar_recomendados(lista_no_vistas,lista_vistas,lista_recomendadas) ;
+                                        llenar_recomendados(lista_no_vistas,lista_vistas,lista_recomendadas) ;
 
-                else
-                     llenar_recomendados(lista_no_vistas,lista_recomendadas) ;
+                                    else
+                                         llenar_recomendados(lista_no_vistas,lista_recomendadas) ;
 
-                mostrar_lista_pelicula(lista_recomendadas) ;
-                }
-                break ;
+                                    mostrar_lista_pelicula(lista_recomendadas) ;
+                                    }
+                                    break ;
 
              case SALIR: cout << MSJ_FIN_PROGRAMA << endl ;
-                break ;
+                         break ;
     }
 }
 
@@ -80,7 +80,6 @@ void Programa:: mostrar_lista_pelicula( Lista<Peliculas*> &lista ){
 }
 
 
-//Lo forma con vistas y no vistas
 void Programa::llenar_recomendados( Lista<Dato> &lista_no_vistas, Lista<Dato> &lista_vistas, Lista<Dato> &lista_recomendado ){
 
     cout << endl << "Formo recomendados con vistas y no vistas" << endl ;
@@ -125,7 +124,6 @@ void Programa::llenar_recomendados( Lista<Dato> &lista_no_vistas, Lista<Dato> &l
     }
 
 }
-
 
 
 bool Programa::puntaje_es_valido (int posicion, Lista<Dato> &lista_no_vistas){
@@ -179,7 +177,7 @@ void Programa::llenar_listas_auxiliares( Lista<Dato> &lista_vistas, Lista<string
     }
 }
 
-//lo forma con no vistas unicamente
+
 void Programa::llenar_recomendados( Lista<Dato> &lista_no_vistas, Lista<Dato> &lista_recomendado ){
 
     cout << endl << "Formo recomendados con no vistas" << endl ;
@@ -193,86 +191,7 @@ void Programa::llenar_recomendados( Lista<Dato> &lista_no_vistas, Lista<Dato> &l
 
 
 
-//------------------------------------------------------------------------------------------------------
-    //NI IDEA SI FUNCIONA, PROBAR MOSTRAR LAS COSAS. AUXILIAR
-   /*cout<<"Lista actores"<<endl;
-    for (int k = 0; k < actores.obtener_tam() ; ++k) { //por qu� en esta no es "<=" como en los otros ?
-        cout<<actores.obtener_dato(k)<<endl;
-    }
-*/
-//-------------------------------------------------------------------------------------------------------
-
-
-
-/*REVISAR ESTA MAL EL FORMATO, LAS CABECERAS*/
-
-
-/*
-//El programa principal deberia empezar llamando la clase LeerArchivo para formar las listas con lo obtenido en el archivo.
-void Programa::programa_principal() {
-
-    //leer_archivo.metodoAdefinir
-    int auxiliar;
-
-    while(auxiliar!=4){
-        cout<<endl<<endl<<"Bienvenido al programa principal"<<endl;
-        cout<<"Decida que operacion quiere hacer"<<endl;
-        cout<<"1. Mostrar lista peliculas vistas"<<endl;
-        cout<<"2. Mostrar lista peliculas no vistas"<<endl;
-        cout<<"3. Mostrar lista peliculas recomendadas"<<endl;
-        cout<<"4. Salir del programa"<<endl;
-        cin>>auxiliar;
-
-        switch (auxiliar) {
-            case 1:mostrar_lista_pelicula(auxiliar);break;
-            case 2:mostrar_lista_pelicula(auxiliar);break;
-            case 3:formar_recomendados(); mostrar_lista_pelicula(auxiliar); break;
-            case 4: cout<<"Fin del programa"<<endl; break;
-            default: {
-                cout<<"Ingrese una opcion valida: 1, 2, 3 o 4"<<endl;
-                cin>>auxiliar;
-            }
-        }
-    }
 
 
 
 
-}
-
-void Programa::mostrar_lista_pelicula(int auxiliar) {
-    //muestra la lista entera (la que le pidas, vista o no vista). El auxiliar te sirve para saber que lista queres que muestre.
-    cout<<"Mostrar lista peliculas"<<endl;
-}
-
-void Programa::formar_recomendados() {
-    //seguramente para hacedr esta van a haber que crear mas metodos (uno que forme recomendados con ambas listas y otro con una sola lista)
-    cout<<"Formando lista recomendados"<<endl;
-}
-
-
-
-
-
-
-
-
-
-//--------------------SETTERS Y GETTERS-----------
-
-void Programa::asignar_archivo_vistas(string archivo_vistas_out) {
-    this->archivo_vistas=archivo_vistas_out;
-}
-
-void Programa::asignar_archivo_novistas(string archivo_novistas_out) {
-    this->archivo_novistas=archivo_novistas_out;
-}
-
-string Programa::obtener_archivo_vistas() {
-    return archivo_vistas;
-}
-
-string Programa::obtener_archivo_novistas() {
-    return archivo_novistas;
-}
-*/
